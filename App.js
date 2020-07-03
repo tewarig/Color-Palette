234abc7dd158f669ed9f64fe380c0b0a335cae52
Gaurav Tewari <gauravtewari111@gmail.com>
2020-07-03 23:20:41 +0530

    styling exersice completed

--- App.js
+++ App.js
@@ -1,22 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
-  console.log("hello world! da da a d a d");
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={styles.pink}>
+      <View style={[styles.container, styles.pink]}>
+        <Text> Here are some boxes of different colors
+        </Text>
+        <Text style={styles.box1}> Cyan #2aa198
+        </Text>
+        <Text style={styles.box2}> Blue #268bd2
+        </Text>
+        <Text style={styles.box3}> Magneta #d33682
+        </Text>
+        <Text style={styles.box4}> Orange #cb4b16
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
+
+};
 
 const styles = StyleSheet.create({
-  container: {
+  pink: {
+    backgroundColor: 'pink',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 25,
+
   },
+
+
+  container: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+  box1: {
+    color: 'white',
+    alignItems: 'center',
+    backgroundColor: '#2aa198',
+    padding: 1,
+    marginBottom: 10,
+    padding: 10,
+    justifyContent: 'center',
+    marginTop: 10,
+    paddingLeft: 130
+  },
+  box2: {
+    color: 'white',
+    alignItems: 'center',
+    padding: 1,
+    marginBottom: 10,
+    padding: 10,
+    justifyContent: 'center',
+    marginTop: 10,
+    paddingLeft: 130,
+    backgroundColor: '#268db2'
+  },
+  box3: {
+    color: 'white',
+    alignItems: 'center',
+    padding: 1,
+    marginBottom: 10,
+    padding: 10,
+    justifyContent: 'center',
+    marginTop: 10,
+    paddingLeft: 130,
+    backgroundColor: '#d33682'
+  },
+  box4: {
+
+    color: 'white',
+    alignItems: 'center',
+    padding: 1,
+    marginBottom: 10,
+    padding: 10,
+    justifyContent: 'center',
+    marginTop: 10,
+    paddingLeft: 130,
+    backgroundColor: '#cb4b16'
+  }
 });
+export default App; 
