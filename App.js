c5c7513935ba3b0dd9e14d72e4caf8f091a1e2e8
Gaurav Tewari <gauravtewari111@gmail.com>
2020-07-03 22:14:03 +0530

    basic bolerplate

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("hello world! da da a d a d");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
