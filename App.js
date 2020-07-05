dbeb1ef549c6a47d741bcd8c0bdfdb963d67ee03
Gaurav Tewari <gauravtewari111@gmail.com>
2020-07-05 18:59:10 +0530

    :-D finally done

--- App.js
+++ App.js
@@ -4,20 +4,41 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/home';
 import ColorPalette from './screens/colorPalette';
+import ColorPaletteModal from './screens/colorPaletteModal';
+const RootStack = createStackNavigator();
+const MainStack = createStackNavigator();
 
-const Stack = createStackNavigator();
+
+
+const MainStackScreen = () => {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name="Home" component={Home} />
+      <MainStack.Screen
+        name="ColorPalette"
+        component={ColorPalette}
+        options={({ route }) => ({ title: route.params.paletteName })}
+      />
+
+    </MainStack.Navigator>
+  );
+};
+
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen
-          name="ColorPalette"
-          component={ColorPalette}
-          options={({ route }) => ({ title: route.params.paletteName })}
+      <RootStack.Navigator mode="modal">
+        <RootStack.Screen
+          name="Main"
+          component={MainStackScreen}
+          options={{ headerShown: false }}
         />
-      </Stack.Navigator>
+        <RootStack.Screen
+          name="ColorPalleteModal"
+          component={ColorPaletteModal}
+        />
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 };
--- screens/home.js
+++ screens/home.js
@@ -1,53 +1,47 @@
-import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, FlatList, RefreshControl, TouchableOpacity, Text } from 'react-native';
 import PalettePreview from '../comp/palettePreview';
 
-const SOLARIZED = [
-    { colorName: 'Base03', hexCode: '#002b36' },
-    { colorName: 'Base02', hexCode: '#073642' },
-    { colorName: 'Base01', hexCode: '#586e75' },
-    { colorName: 'Base00', hexCode: '#657b83' },
-    { colorName: 'Base0', hexCode: '#839496' },
-    { colorName: 'Base1', hexCode: '#93a1a1' },
-    { colorName: 'Base2', hexCode: '#eee8d5' },
-    { colorName: 'Base3', hexCode: '#fdf6e3' },
-    { colorName: 'Yellow', hexCode: '#b58900' },
-    { colorName: 'Orange', hexCode: '#cb4b16' },
-    { colorName: 'Red', hexCode: '#dc322f' },
-    { colorName: 'Magenta', hexCode: '#d33682' },
-    { colorName: 'Violet', hexCode: '#6c71c4' },
-    { colorName: 'Blue', hexCode: '#268bd2' },
-    { colorName: 'Cyan', hexCode: '#2aa198' },
-    { colorName: 'Green', hexCode: '#859900' },
-];
+const URL = 'https://color-palette-api.kadikraman.now.sh/palettes';
 
-const RAINBOW = [
-    { colorName: 'Red', hexCode: '#FF0000' },
-    { colorName: 'Orange', hexCode: '#FF7F00' },
-    { colorName: 'Yellow', hexCode: '#FFFF00' },
-    { colorName: 'Green', hexCode: '#00FF00' },
-    { colorName: 'Violet', hexCode: '#8B00FF' },
-];
 
-const FRONTEND_MASTERS = [
-    { colorName: 'Red', hexCode: '#c02d28' },
-    { colorName: 'Black', hexCode: '#3e3e3e' },
-    { colorName: 'Grey', hexCode: '#8a8a8a' },
-    { colorName: 'White', hexCode: '#ffffff' },
-    { colorName: 'Orange', hexCode: '#e66225' },
-];
 
-const COLOR_PALETTES = [
-    { paletteName: 'Solarized', colors: SOLARIZED },
-    { paletteName: 'Frontend Masters', colors: FRONTEND_MASTERS },
-    { paletteName: 'Rainbow', colors: RAINBOW },
-];
+const Home = ({ navigation, route }) => {
+    const newColorPalette = route.params ? route.params.newColorPalette : undefined;
 
-const Home = ({ navigation }) => {
+    const [palettes, setPalettes] = useState([]);
+    const [isRefreshing, setIsRefreshing] = useState(false);
+
+    const handleFetchPalettes = useCallback(async () => {
+        const response = await fetch(URL);
+        if (response.ok) {
+            const palettes = await response.json();
+            setPalettes(palettes);
+        }
+    }, []);
+
+    useEffect(() => {
+        handleFetchPalettes();
+    }, []);
+    useEffect(() => {
+        if (newColorPalette) {
+            setPalettes(palettes => [newColorPalette, ...palettes])
+        }
+    }, [newColorPalette]);
+
+
+    const handleRefresh = useCallback(async () => {
+        setIsRefreshing(true);
+        await handleFetchPalettes();
+        setTimeout(() => {
+            setIsRefreshing(false);
+        }, 1000);
+    });
+
     return (
         <FlatList
             style={styles.list}
-            data={COLOR_PALETTES}
+            data={palettes}
             keyExtractor={item => item.paletteName}
             renderItem={({ item }) => (
                 <PalettePreview
@@ -55,6 +49,14 @@ const Home = ({ navigation }) => {
                     palette={item}
                 />
             )}
+            refreshControl={
+                <RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh}
+                />
+
+            }
+
+            ListHeaderComponent={<TouchableOpacity onPress={() => { navigation.navigate('ColorPalleteModal'); }}>
+                <Text style={styles.buttonText}>  Make your own color scheme  </Text></TouchableOpacity>}
         />
     );
 };
@@ -64,7 +66,12 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         backgroundColor: 'white',
-    },
+    }, buttonText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'teal',
+        marginBottom: 10
+    }
 });
 
 export default Home;
